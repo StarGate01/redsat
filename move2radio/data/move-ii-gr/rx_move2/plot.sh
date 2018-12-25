22fc4c3b14d08452d2c5484f66c8a236f1e55756
Alexander Ulanowski <github-alu96@a1u.cc>
2018-12-25 20:28:45 +0100

    plotting script does not override old images any more

--- move2radio/data/move-ii-gr/rx_move2/plot.sh
+++ move2radio/data/move-ii-gr/rx_move2/plot.sh
@@ -1,1 +1,1 @@
-gnuplot -e "inputfile='$1'" -e "outfile='waterfall.png'" -e "width=1000" -e "height=2000" $2 satnogs_waterfall.gp
+gnuplot -e "inputfile='$1'" -e "outfile='$1.png'" -e "width=1000" -e "height=2000" $2 satnogs_waterfall.gp
