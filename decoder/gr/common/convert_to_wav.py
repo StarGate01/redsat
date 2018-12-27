c262670c172ee14a1872bdc1b6481c57504c37ab
Alexander Ulanowski <github-alu96@a1u.cc>
2018-12-27 18:33:48 +0100

    moved blocks to gr/common

--- decoder/gr/common/convert_to_wav.py
+++ decoder/gr/common/convert_to_wav.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Create Waterfall
-# Generated: Thu Dec 27 12:51:28 2018
+# Title: Convert To Wav
+# Generated: Thu Dec 27 14:31:42 2018
 ##################################################
 
 import os
@@ -11,6 +11,7 @@ import sys
 sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
 
 from file_source import file_source  # grc-generated hier_block
+from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
@@ -19,13 +20,12 @@ from optparse import OptionParser
 from os.path import splitext
 from range_selector import range_selector  # grc-generated hier_block
 import ConfigParser
-import satnogs
 
 
-class create_waterfall(gr.top_block):
+class convert_to_wav(gr.top_block):
 
     def __init__(self, filename="", keep=0, skip=0):
-        gr.top_block.__init__(self, "Create Waterfall")
+        gr.top_block.__init__(self, "Convert To Wav")
 
         ##################################################
         # Parameters
@@ -39,7 +39,7 @@ class create_waterfall(gr.top_block):
         ##################################################
         self._meta_samp_rate_config = ConfigParser.ConfigParser()
         self._meta_samp_rate_config.read(filename)
-        try: meta_samp_rate = self._meta_samp_rate_config.getfloat("main", "samp_rate")
+        try: meta_samp_rate = self._meta_samp_rate_config.getfloat('main', 'samp_rate')
         except: meta_samp_rate = 0
         self.meta_samp_rate = meta_samp_rate
         self.filename_base = filename_base = splitext(filename)[0]
@@ -47,51 +47,42 @@ class create_waterfall(gr.top_block):
         ##################################################
         # Blocks
         ##################################################
-        self.satnogs_waterfall_sink_0_0 = satnogs.waterfall_sink(meta_samp_rate, 0.0, 15, 4096, filename_base + ".d.wf", 1)
-        self.satnogs_waterfall_sink_0 = satnogs.waterfall_sink(meta_samp_rate, 0.0, 15, 4096, filename_base + ".nd.wf", 1)
-        self.range_selector_0_0 = range_selector(
-            samp_rate=meta_samp_rate,
-            skip=skip,
-            keep=keep,
-        )
         self.range_selector_0 = range_selector(
+            keep=keep,
             samp_rate=meta_samp_rate,
             skip=skip,
-            keep=keep,
-        )
-        self.file_source_0_0 = file_source(
-            p_doppler_correct=0,
-            p_meta_file=filename,
-            p_realtime=False,
         )
         self.file_source_0 = file_source(
             p_doppler_correct=10,
             p_meta_file=filename,
             p_realtime=False,
         )
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(filename_base + ".wav", 2, int(meta_samp_rate), 8)
+        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
+
+
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.file_source_0, 0), (self.range_selector_0, 0))    
-        self.connect((self.file_source_0_0, 0), (self.range_selector_0_0, 0))    
-        self.connect((self.range_selector_0, 0), (self.satnogs_waterfall_sink_0_0, 0))    
-        self.connect((self.range_selector_0_0, 0), (self.satnogs_waterfall_sink_0, 0))    
+        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_wavfile_sink_0, 1))
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_wavfile_sink_0, 0))
+        self.connect((self.file_source_0, 0), (self.range_selector_0, 0))
+        self.connect((self.range_selector_0, 0), (self.blocks_complex_to_float_0, 0))
 
     def get_filename(self):
         return self.filename
 
     def set_filename(self, filename):
         self.filename = filename
-        self.set_filename_base(splitext(self.filename)[0])
         self._meta_samp_rate_config = ConfigParser.ConfigParser()
         self._meta_samp_rate_config.read(self.filename)
-        if not self._meta_samp_rate_config.has_section("main"):
-        	self._meta_samp_rate_config.add_section("main")
-        self._meta_samp_rate_config.set("main", "samp_rate", str(None))
+        if not self._meta_samp_rate_config.has_section('main'):
+        	self._meta_samp_rate_config.add_section('main')
+        self._meta_samp_rate_config.set('main', 'samp_rate', str(None))
         self._meta_samp_rate_config.write(open(self.filename, 'w'))
+        self.set_filename_base(splitext(self.filename)[0])
         self.file_source_0.set_p_meta_file(self.filename)
-        self.file_source_0_0.set_p_meta_file(self.filename)
 
     def get_keep(self):
         return self.keep
@@ -99,7 +90,6 @@ class create_waterfall(gr.top_block):
     def set_keep(self, keep):
         self.keep = keep
         self.range_selector_0.set_keep(self.keep)
-        self.range_selector_0_0.set_keep(self.keep)
 
     def get_skip(self):
         return self.skip
@@ -107,7 +97,6 @@ class create_waterfall(gr.top_block):
     def set_skip(self, skip):
         self.skip = skip
         self.range_selector_0.set_skip(self.skip)
-        self.range_selector_0_0.set_skip(self.skip)
 
     def get_meta_samp_rate(self):
         return self.meta_samp_rate
@@ -115,17 +104,17 @@ class create_waterfall(gr.top_block):
     def set_meta_samp_rate(self, meta_samp_rate):
         self.meta_samp_rate = meta_samp_rate
         self.range_selector_0.set_samp_rate(self.meta_samp_rate)
-        self.range_selector_0_0.set_samp_rate(self.meta_samp_rate)
 
     def get_filename_base(self):
         return self.filename_base
 
     def set_filename_base(self, filename_base):
         self.filename_base = filename_base
+        self.blocks_wavfile_sink_0.open(self.filename_base + ".wav")
 
 
 def argument_parser():
-    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
     parser.add_option(
         "-f", "--filename", dest="filename", type="string", default="",
         help="Set filename [default=%default]")
@@ -138,7 +127,7 @@ def argument_parser():
     return parser
 
 
-def main(top_block_cls=create_waterfall, options=None):
+def main(top_block_cls=convert_to_wav, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
--- decoder/gr/common/create_waterfall.py
+++ decoder/gr/common/create_waterfall.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Convert To Wav
-# Generated: Thu Dec 27 12:51:26 2018
+# Title: Create Waterfall
+# Generated: Thu Dec 27 14:01:36 2018
 ##################################################
 
 import os
@@ -11,7 +11,6 @@ import sys
 sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
 
 from file_source import file_source  # grc-generated hier_block
-from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
@@ -20,12 +19,13 @@ from optparse import OptionParser
 from os.path import splitext
 from range_selector import range_selector  # grc-generated hier_block
 import ConfigParser
+import satnogs
 
 
-class convert_to_wav(gr.top_block):
+class create_waterfall(gr.top_block):
 
     def __init__(self, filename="", keep=0, skip=0):
-        gr.top_block.__init__(self, "Convert To Wav")
+        gr.top_block.__init__(self, "Create Waterfall")
 
         ##################################################
         # Parameters
@@ -47,26 +47,36 @@ class convert_to_wav(gr.top_block):
         ##################################################
         # Blocks
         ##################################################
+        self.satnogs_waterfall_sink_0_0 = satnogs.waterfall_sink(meta_samp_rate, 0.0, 15, 4096, filename_base + ".d.wf", 1)
+        self.satnogs_waterfall_sink_0 = satnogs.waterfall_sink(meta_samp_rate, 0.0, 15, 4096, filename_base + ".nd.wf", 1)
+        self.range_selector_0_0 = range_selector(
+            samp_rate=meta_samp_rate,
+            skip=skip,
+            keep=keep,
+        )
         self.range_selector_0 = range_selector(
             samp_rate=meta_samp_rate,
             skip=skip,
             keep=keep,
+        )
+        self.file_source_0_0 = file_source(
+            p_doppler_correct=0,
+            p_meta_file=filename,
+            p_realtime=False,
         )
         self.file_source_0 = file_source(
             p_doppler_correct=10,
             p_meta_file=filename,
             p_realtime=False,
         )
-        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(filename_base + ".wav", 2, meta_samp_rate, 8)
-        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_wavfile_sink_0, 1))    
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_wavfile_sink_0, 0))    
         self.connect((self.file_source_0, 0), (self.range_selector_0, 0))    
-        self.connect((self.range_selector_0, 0), (self.blocks_complex_to_float_0, 0))    
+        self.connect((self.file_source_0_0, 0), (self.range_selector_0_0, 0))    
+        self.connect((self.range_selector_0, 0), (self.satnogs_waterfall_sink_0_0, 0))    
+        self.connect((self.range_selector_0_0, 0), (self.satnogs_waterfall_sink_0, 0))    
 
     def get_filename(self):
         return self.filename
@@ -81,6 +91,7 @@ class convert_to_wav(gr.top_block):
         self._meta_samp_rate_config.set("main", "samp_rate", str(None))
         self._meta_samp_rate_config.write(open(self.filename, 'w'))
         self.file_source_0.set_p_meta_file(self.filename)
+        self.file_source_0_0.set_p_meta_file(self.filename)
 
     def get_keep(self):
         return self.keep
@@ -88,6 +99,7 @@ class convert_to_wav(gr.top_block):
     def set_keep(self, keep):
         self.keep = keep
         self.range_selector_0.set_keep(self.keep)
+        self.range_selector_0_0.set_keep(self.keep)
 
     def get_skip(self):
         return self.skip
@@ -95,6 +107,7 @@ class convert_to_wav(gr.top_block):
     def set_skip(self, skip):
         self.skip = skip
         self.range_selector_0.set_skip(self.skip)
+        self.range_selector_0_0.set_skip(self.skip)
 
     def get_meta_samp_rate(self):
         return self.meta_samp_rate
@@ -102,13 +115,13 @@ class convert_to_wav(gr.top_block):
     def set_meta_samp_rate(self, meta_samp_rate):
         self.meta_samp_rate = meta_samp_rate
         self.range_selector_0.set_samp_rate(self.meta_samp_rate)
+        self.range_selector_0_0.set_samp_rate(self.meta_samp_rate)
 
     def get_filename_base(self):
         return self.filename_base
 
     def set_filename_base(self, filename_base):
         self.filename_base = filename_base
-        self.blocks_wavfile_sink_0.open(self.filename_base + ".wav")
 
 
 def argument_parser():
@@ -125,7 +138,7 @@ def argument_parser():
     return parser
 
 
-def main(top_block_cls=convert_to_wav, options=None):
+def main(top_block_cls=create_waterfall, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
