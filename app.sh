02e9c4c0b17bb8ba97a7ac874b7e44ef1584622f
Christoph Honal <christoph.honal@web.de>
2018-12-30 10:48:34 +0100

    adapt monitor to arch

--- app.sh
+++ app.sh
@@ -5,31 +5,41 @@ STACKS_COMMON="frontend radio"
 STACKS_SERVER="frontend monitor"
 STACKS="frontend monitor radio"
 
+WSL=`uname -r | grep "Microsoft"`
+ARCH=`uname -m`
+if [ -z "$WSL" ]; then
+    if [ "$ARCH" == "armv7l" ]; then
+        echo "System: Raspberry"
+        SYSC="-rpi"
+    else
+        echo "System: Default"
+        SYSC=""
+    fi
+else
+    echo "System: WSL"
+    SYSC="-wsl"
+fi
+
 if [ "$1" == "up" ]; then
     echo "Starting REDSAT"
     echo "Creating networks"
     for NETWORK in $NETWORKS; do docker network create $NETWORK; done
     echo "Starting stacks"
     cd frontend && docker-compose up -d && cd ..
-    WSL=`uname -r | grep "Microsoft"`
-    if [ -z "$WSL" ]; then
-        echo "Starting monitoring stack"
-        cd monitor && docker-compose up -d && cd ..
-    else
-        echo "Starting monitoring stack (WSL)"
-        cd monitor && docker-compose -f docker-compose-wsl.yml up -d && cd ..
-    fi
+    cd monitor && docker-compose -f docker-compose$SYSC.yml up -d && cd ..
     cd radio && docker-compose up -d scheduler && cd ..
 elif [ "$1" == "down" ]; then
     echo "Stopping REDSAT"
     echo "Stopping stacks"
-    for STACK in $STACKS; do cd $STACK && docker-compose down && cd ..; done
+    for STACK in $STACKS_COMMON; do cd $STACK && docker-compose down && cd ..; done
+    cd monitor && docker-compose -f docker-compose$SYSC.yml down && cd ..
     echo "Removing networks"
     for NETWORK in $NETWORKS; do docker network remove $NETWORK; done
 elif [ "$1" == "build" ]; then
     ARCH=`uname -m`
-    echo "Building REDSAT for $ARCH"
-    for STACK in $STACKS; do cd $STACK && docker-compose build && cd ..; done
+    echo "Building REDSAT for $ARCH$SYSC"
+    for STACK in $STACKS_COMMON; do cd $STACK && docker-compose build && cd ..; done
+    cd monitor && docker-compose -f docker-compose$SYSC.yml build && cd ..
 elif [ "$1" == "radio" ]; then
     echo "Running radio component: $2"
     cd radio && docker-compose run --rm $2 /app/run.sh "${@:3}" && cd ..
