e2ec520850a10cd53238fd51ecbc1043d27d819c
Alexander Ulanowski <github-alu96@a1u.cc>
2019-01-20 15:46:15 +0100

    updated predict.py to show timestamps and optionally local times

--- radio/scheduler/data/predict.py
+++ radio/scheduler/data/predict.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import sys
+from sys import argv
 import os
 import ephem
 import math
-import datetime
-from time import gmtime, strftime
+from time import gmtime, localtime, strftime, strptime
+from calendar import timegm
+import argparse
+
+parser = argparse.ArgumentParser(description='Predicts next satellite overpasses.')
+parser.add_argument('--path', type=str, default="/app/config/eleme", help='Path to TLE directory')
+parser.add_argument('--local', dest='local', action='store_const', const=True, default=False, help='Show local times')
+args = parser.parse_args()
 
 nowdate = strftime('%Y/%m/%d %H:%M:%S', gmtime())
 
@@ -15,18 +21,31 @@ myloc.lon = os.environ['LON']
 myloc.lat = os.environ['LAT']
 myloc.elevation = int(os.environ['ELV'])
 
-for subdir, dirs, files in os.walk('/app/config/elements'):
+fmt_date = lambda ts: strftime("%Y/%m/%d %H:%M:%S", localtime(ts) if args.local else gmtime(ts))
+
+for subdir, dirs, files in os.walk(args.path):
     for file in files:
-        print file
+#        print file
         with open(os.path.join(subdir, file), 'r') as f:
             data = f.readlines()
-            mysat = ephem.readtle(data[0], data[1], data[2])
+            if len(data) < 3:
+                print("warning: TLE file {} does not contain three lines and will not be used for prediction".format(file))
+                continue
+
+            name = data[0]
+            mysat = ephem.readtle(name, data[1], data[2])
 
             myloc.date = nowdate
             for i in range(3):
                 start_time, aos_az, mid_time, max_elv, end_time, los_az = myloc.next_pass(mysat)
-                print "%s - %s - %s" % (start_time, mid_time, end_time)
-                print "  AOS Az: %4.2f° LOS Az: %4.2f° Max Elv: %4.2f°" % \
+		start_ts = timegm(strptime(str(start_time), '%Y/%m/%d %H:%M:%S'))
+                mid_ts = timegm(strptime(str(mid_time), '%Y/%m/%d %H:%M:%S'))
+                end_ts = timegm(strptime(str(end_time), '%Y/%m/%d %H:%M:%S'))
+                duration = end_ts - start_ts
+                print "%s, %d, %d \n | %s - %s - %s" % (name.strip(), start_ts, duration, fmt_date(start_ts), fmt_date(mid_ts), fmt_date(end_ts))
+                #print mktime(\
+                #    datetime.strptime(str(start_time), '%Y/%m/%d %H:%M:%S').timetuple())
+	        print "   AOS Az: %4.2f° LOS Az: %4.2f° Max Elv: %4.2f°" % \
                     (math.degrees(aos_az), math.degrees(los_az), math.degrees(max_elv))
                 myloc.date = end_time + ephem.minute
             print
