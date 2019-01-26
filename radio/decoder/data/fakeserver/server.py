918e0859f9aa5abc45a9acedf9c4a9f28ead985e
Alexander Ulanowski <github-alu96@a1u.cc>
2019-01-26 13:06:17 +0100

    rewritten fakeserver

--- radio/decoder/data/fakeserver/server.py
+++ radio/decoder/data/fakeserver/server.py
@@ -1,28 +1,64 @@
-import socket
+from sys import argv
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import json
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 8000
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+class FakeServer(BaseHTTPRequestHandler):
+    def _set_response(self, code=200):
+        self.send_response(code)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
 
-with open("/app/fakeserver/response.html") as f:
-	html = f.read()
+    def do_GET(self):
+        logging.info("GET request, Path: %s\n", str(self.path))
+        
+        code = 404
+        reponse = '{}'
+        if self.path == "/session":
+        	code = 200
+        	response = '{"session":"15700771d1262b8e"}'
+        
+        self._set_response(code=code)
+        self.wfile.write(response.encode('utf-8'))
 
-while 1: 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.bind((TCP_IP, TCP_PORT))
-	s.listen(1)
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
 
-	conn, addr = s.accept()
-	print 'MOVE2-Fakeserver: Connection: ' + addr
+        logging.info("POST request, Path: %s\nBody:\n%s\n",
+                str(self.path), post_data.decode('utf-8'))
 
-	try:
-		while 1:
-			conn.send(html)
-			data = conn.recv(BUFFER_SIZE)
-			if not data: break
-			else: print "MOVE2-Fakeserver: Data: " + data
-	except:
-		pass
+        code = 404
+        response = '{}'
 
-	conn.close()
+        if self.path == '/nanolink':
+        	data = json.loads(post_data.decode('utf-8'))
+        	logging.info("DATA: " + data['nanolink_frame'])
+        	global frames
+        	frames.write(data['nanolink_frame'] + '\n')
+        	frames.flush()
+        	code = 200
+
+        self._set_response(code=code)
+        self.wfile.write(response.encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=FakeServer, port=8000):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+	file = 'frames.txt'
+	if len(argv) > 1:
+		file = argv[1]
+
+	with open(file, "a") as frames:
+		frames.write('\n')
+		run()
