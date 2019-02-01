95a7cc57c04e2158287fa41e9ff162b80f1539c3
Alexander Ulanowski <github-alu96@a1u.cc>
2019-02-01 22:32:34 +0100

    move 2 decoder: added gui and realtime option

--- radio/decoder/data/gr/test_receiver.py
+++ radio/decoder/data/gr/test_receiver.py
@@ -7,40 +7,9 @@
 # Generated: Wed Nov 21 11:02:04 2018
 ##################################################
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-
-from PyQt4 import Qt
-from gnuradio import analog
-from gnuradio import blocks
-from gnuradio import digital
-from gnuradio import digital;import cmath
-from gnuradio import eng_notation
-from gnuradio import filter
-from gnuradio import gr
-from gnuradio import qtgui
-from gnuradio.eng_option import eng_option
-from gnuradio.filter import firdes
-from gnuradio.qtgui import Range, RangeWidget
-from optparse import OptionParser
-import ccsds
-import math
-import osmosdr
-import sip
 import sys
-import time
-from gnuradio import qtgui
-
 from os.path import splitext
 from ConfigParser import ConfigParser
-
 
 _meta_file = sys.argv[1] 
 _file = splitext(_meta_file)[0] + ".wav"
@@ -58,32 +27,75 @@ _submit = False
 if '--submit' in sys.argv:
     _submit = True
 
+_gui = False
+if '--gui' in sys.argv:
+    _gui = True
+
+# realtime processing flag
+_rt = False
+if '--rt' in sys.argv:
+    _rt = True
+
+
+if __name__ == '__main__' and _gui:
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import digital;import cmath
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+if _gui:
+    from gnuradio import qtgui
+    from gnuradio.qtgui import Range, RangeWidget
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import ccsds
+import math
+import osmosdr
+import sip
+import time
+
 
 class downlink_em(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Downlink for EM")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Downlink for EM")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "downlink_em")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+        if _gui:
+            Qt.QWidget.__init__(self)
+            self.setWindowTitle("Downlink for EM")
+            qtgui.util.check_set_qss()
+            try:
+                self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+            except:
+                pass
+            
+            self.top_scroll_layout = Qt.QVBoxLayout()
+            self.setLayout(self.top_scroll_layout)
+            self.top_scroll = Qt.QScrollArea()
+            self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+            self.top_scroll_layout.addWidget(self.top_scroll)
+            self.top_scroll.setWidgetResizable(True)
+            self.top_widget = Qt.QWidget()
+            self.top_scroll.setWidget(self.top_widget)
+            self.top_layout = Qt.QVBoxLayout(self.top_widget)
+            self.top_grid_layout = Qt.QGridLayout()
+            self.top_layout.addLayout(self.top_grid_layout)
+
+            self.settings = Qt.QSettings("GNU Radio", "downlink_em")
+            self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
 
         ##################################################
@@ -102,89 +114,93 @@ class downlink_em(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self._frequency_offset_correction_range = Range(-120000, 120000, 100, frequency_offset_correction, 200)
-        self._frequency_offset_correction_win = RangeWidget(self._frequency_offset_correction_range, self.set_frequency_offset_correction, 'Frequency Correction', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._frequency_offset_correction_win, 0, 0, 1, 1)
-        for r in range(0, 1):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
+        if _gui:
+            self._frequency_offset_correction_range = Range(-120000, 120000, 100, frequency_offset_correction, 200)
+            self._frequency_offset_correction_win = RangeWidget(self._frequency_offset_correction_range, self.set_frequency_offset_correction, 'Frequency Correction', "counter_slider", float)
+            self.top_grid_layout.addWidget(self._frequency_offset_correction_win, 0, 0, 1, 1)
+            for r in range(0, 1):
+                self.top_grid_layout.setRowStretch(r, 1)
+            for c in range(0, 1):
+                self.top_grid_layout.setColumnStretch(c, 1)
+                
         self.rational_resampler_xxx_1 = filter.rational_resampler_ccc(
                 interpolation=1,
                 decimation=self.oversample,
                 taps=None,
                 fractional_bw=None,
         )
-        self.qtgui_sink_x_0_0_1 = qtgui.sink_c(
-        	1024, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	145.79e6, #fc
-        	samp_rate*oversample, #bw - 1??
-        	"Vor Sync", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
-        )
-        self.qtgui_sink_x_0_0_1.set_update_time(1.0/10)
-        self._qtgui_sink_x_0_0_1_win = sip.wrapinstance(self.qtgui_sink_x_0_0_1.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_1_win, 1, 0, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
+
+        if _gui:
+            self.qtgui_sink_x_0_0_1 = qtgui.sink_c(
+            	1024, #fftsize
+            	firdes.WIN_BLACKMAN_hARRIS, #wintype
+            	145.79e6, #fc
+            	samp_rate*oversample, #bw - 1??
+            	"Vor Sync", #name
+            	True, #plotfreq
+            	True, #plotwaterfall
+            	True, #plottime
+            	True, #plotconst
+            )
+            self.qtgui_sink_x_0_0_1.set_update_time(1.0/10)
+            self._qtgui_sink_x_0_0_1_win = sip.wrapinstance(self.qtgui_sink_x_0_0_1.pyqwidget(), Qt.QWidget)
+            self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_1_win, 1, 0, 1, 1)
+            for r in range(1, 2):
+                self.top_grid_layout.setRowStretch(r, 1)
+            for c in range(0, 1):
+                self.top_grid_layout.setColumnStretch(c, 1)
 
 
-        self.qtgui_sink_x_0_0_1.enable_rf_freq(False)
-
-
-
-        self.qtgui_sink_x_0_0_0_0 = qtgui.sink_c(
-        	1024, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	samp_rate/oversample, #bw
-        	"Vor Sync", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
-        )
-        self.qtgui_sink_x_0_0_0_0.set_update_time(1.0/10)
-        self._qtgui_sink_x_0_0_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0_0_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_0_0_win, 2, 0, 1, 1)
-        for r in range(2, 3):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
-
-
-        self.qtgui_sink_x_0_0_0_0.enable_rf_freq(False)
+            self.qtgui_sink_x_0_0_1.enable_rf_freq(False)
 
 
 
-        self.qtgui_sink_x_0_0_0 = qtgui.sink_c(
-        	1024, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	samp_rate/oversample, #bw
-        	"Vor Sync", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
-        )
-        self.qtgui_sink_x_0_0_0.set_update_time(1.0/10)
-        self._qtgui_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_0_win, 3, 0, 1, 1)
-        for r in range(3, 4):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
+            self.qtgui_sink_x_0_0_0_0 = qtgui.sink_c(
+            	1024, #fftsize
+            	firdes.WIN_BLACKMAN_hARRIS, #wintype
+            	0, #fc
+            	samp_rate/oversample, #bw
+            	"Vor Sync", #name
+            	True, #plotfreq
+            	True, #plotwaterfall
+            	True, #plottime
+            	True, #plotconst
+            )
+            self.qtgui_sink_x_0_0_0_0.set_update_time(1.0/10)
+            self._qtgui_sink_x_0_0_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0_0_0.pyqwidget(), Qt.QWidget)
+            self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_0_0_win, 2, 0, 1, 1)
+            for r in range(2, 3):
+                self.top_grid_layout.setRowStretch(r, 1)
+            for c in range(0, 1):
+                self.top_grid_layout.setColumnStretch(c, 1)
 
 
-        self.qtgui_sink_x_0_0_0.enable_rf_freq(False)
+            self.qtgui_sink_x_0_0_0_0.enable_rf_freq(False)
 
+
+
+            self.qtgui_sink_x_0_0_0 = qtgui.sink_c(
+            	1024, #fftsize
+            	firdes.WIN_BLACKMAN_hARRIS, #wintype
+            	0, #fc
+            	samp_rate/oversample, #bw
+            	"Vor Sync", #name
+            	True, #plotfreq
+            	True, #plotwaterfall
+            	True, #plottime
+            	True, #plotconst
+            )
+            self.qtgui_sink_x_0_0_0.set_update_time(1.0/10)
+            self._qtgui_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0_0.pyqwidget(), Qt.QWidget)
+            self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_0_win, 3, 0, 1, 1)
+            for r in range(3, 4):
+                self.top_grid_layout.setRowStretch(r, 1)
+            for c in range(0, 1):
+                self.top_grid_layout.setColumnStretch(c, 1)
+
+
+            self.qtgui_sink_x_0_0_0.enable_rf_freq(False)
+        # EOF if _gui
 
         # self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
         # self.osmosdr_source_0.set_sample_rate(samp_rate*oversample)
@@ -201,18 +217,19 @@ class downlink_em(gr.top_block, Qt.QWidget):
         
         
         self.blocks_wavfile_source_0 = blocks.wavfile_source(_file, _repeat)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         
-        
+        input_src = (self.blocks_float_to_complex_0, 0)
+
         self.blocks_message_debug_1_0_0 = blocks.message_debug()
         self.ccsds_message_info_0 = ccsds.message_info("Block received and sent to Nanolink: ", 20)
 
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate*oversample,True)
 
         self.digital_mpsk_receiver_cc_0_0 = digital.mpsk_receiver_cc(2, 0, 31.41e-3, -0.01, 0.01, 0.25, 0.05, samp_rate/self.oversample/12.5e3, 0.01, 0.005)
         self.digital_constellation_decoder_cb_0 = digital.constellation_decoder_cb(variable_constellation_0)
         self.ccsds_softbits_msg_to_bytes_b_0 = ccsds.softbits_msg_to_bytes_b()
-        self.ccsds_send_nanolink_0 = ccsds.send_nanolink("http://move2radio.lrt.mw.tum.de" if _submit else "http://localhost:8000")
+        self.ccsds_send_nanolink_0 = ccsds.send_nanolink("https://move2radio.lrt.mw.tum.de" if _submit else "http://localhost:8000")
         self.ccsds_randomiser_softbits_0 = ccsds.randomiser_softbits(0x95,0xFF)
         self.ccsds_mpsk_ambiguity_resolver_f_0 = ccsds.mpsk_ambiguity_resolver_f(2,'49E0DCC7',32,1,0.8,block_len_enc,0)
         self.ccsds_ldpc_decoder_0 = ccsds.ldpc_decoder('/tmp/AR4JA_r12_k1024n.a', ccsds.LDPC_SYS_FRONT, ccsds.LDPC_PUNCT_BACK, 512, tuple(([])))
@@ -234,8 +251,14 @@ class downlink_em(gr.top_block, Qt.QWidget):
         # Connections
         ##################################################
         self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_float_to_complex_0, 1))
-        self.connect((self.blocks_wavfile_source_0, 1), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_wavfile_source_0, 1), (self.blocks_float_to_complex_0, 0))        
+
+
+	if _rt:
+            self.connect(input_src, (self.blocks_throttle_0, 0))
+            self.connect((self.blocks_throttle_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        else:
+            self.connect(input_src, (self.blocks_multiply_xx_0_0, 0))
 
         self.msg_connect((self.ccsds_blob_msg_sink_b_0, 'out'), (self.blocks_message_debug_1_0_0, 'print_pdu'))
         self.msg_connect((self.ccsds_blob_msg_sink_b_0, 'out'), (self.ccsds_message_info_0, 'in'))
@@ -245,7 +268,8 @@ class downlink_em(gr.top_block, Qt.QWidget):
         self.msg_connect((self.ccsds_ldpc_decoder_0, 'out'), (self.ccsds_softbits_msg_to_bytes_b_0, 'in'))
         self.msg_connect((self.ccsds_mpsk_ambiguity_resolver_f_0, 'out'), (self.ccsds_randomiser_softbits_0, 'in'))
         self.msg_connect((self.ccsds_randomiser_softbits_0, 'out'), (self.ccsds_ldpc_decoder_0, 'in'))
-        self.connect((self.analog_agc_xx_0, 0), (self.qtgui_sink_x_0_0_0_0, 0))
+        if _gui:
+            self.connect((self.analog_agc_xx_0, 0), (self.qtgui_sink_x_0_0_0_0, 0))
         self.connect((self.analog_agc_xx_0, 0), (self.rational_resampler_xxx_1, 0))
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 1))
@@ -254,22 +278,25 @@ class downlink_em(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_char_to_float_0, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.analog_agc_xx_0, 0))
         self.connect((self.blocks_multiply_xx_0_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.blocks_multiply_xx_0_0, 0), (self.qtgui_sink_x_0_0_1, 0))
+        if _gui:
+            self.connect((self.blocks_multiply_xx_0_0, 0), (self.qtgui_sink_x_0_0_1, 0))
         self.connect((self.ccsds_softbits_msg_to_bytes_b_0, 0), (self.ccsds_blob_msg_sink_b_0, 0))
         self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blocks_char_to_float_0, 0))
         self.connect((self.digital_mpsk_receiver_cc_0_0, 0), (self.digital_constellation_decoder_cb_0, 0))
-        self.connect((self.digital_mpsk_receiver_cc_0_0, 0), (self.qtgui_sink_x_0_0_0, 0))
+        if _gui:
+            self.connect((self.digital_mpsk_receiver_cc_0_0, 0), (self.qtgui_sink_x_0_0_0, 0))
         #self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0_0, 0))
 
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_multiply_xx_0_0, 0))
+
         #self.connect((self.blocks_throttle_0, 0), (self.qtgui_sink_x_0_0, 0))
 
         self.connect((self.rational_resampler_xxx_1, 0), (self.digital_mpsk_receiver_cc_0_0, 0))
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "downlink_em")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
+        if _gui:
+            self.settings = Qt.QSettings("GNU Radio", "downlink_em")
+            self.settings.setValue("geometry", self.saveGeometry())
+            event.accept()
 
     def get_block_len_enc(self):
         return self.block_len_enc
@@ -289,9 +316,10 @@ class downlink_em(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_sink_x_0_0_1.set_frequency_range(145.79e6, self.samp_rate*self.oversample) # - 1??
-        self.qtgui_sink_x_0_0_0_0.set_frequency_range(0, self.samp_rate/self.oversample)
-        self.qtgui_sink_x_0_0_0.set_frequency_range(0, self.samp_rate/self.oversample)
+        if _gui:
+            self.qtgui_sink_x_0_0_1.set_frequency_range(145.79e6, self.samp_rate*self.oversample) # - 1??
+            self.qtgui_sink_x_0_0_0_0.set_frequency_range(0, self.samp_rate/self.oversample)
+            self.qtgui_sink_x_0_0_0.set_frequency_range(0, self.samp_rate/self.oversample)
         #self.osmosdr_source_0.set_sample_rate(self.samp_rate*self.oversample)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate*self.oversample)
         self.digital_mpsk_receiver_cc_0_0.set_omega(self.samp_rate/self.oversample/12.5e3)
@@ -324,21 +352,10 @@ class downlink_em(gr.top_block, Qt.QWidget):
 
 def main(top_block_cls=downlink_em, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-    tb.start(2080)
-    tb.show()
+    tb.start()
+    tb.wait()
 
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
 
 
 if __name__ == '__main__':
